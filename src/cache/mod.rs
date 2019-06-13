95e42960da87ed99ebc17dafb93fee00aa7a236e
MaaniBeigy <manibeygi@gmail.com>
2019-06-13 19:54:00 +0430

    Improve format/structure :art:

---
+++ src/cache/mod.rs
@@ -0,0 +1,12 @@
+// palantir
+// HTTP REST API reverse proxy
+// Copyright: 2018, Maani Beigy <manibeygi@gmail.com>, 
+//                  AASAAM Software Group <info@aasaam.com>
+// License: MIT/Apache-2.0
+//! # palantir
+//!
+//! `palantir` is a HTTP REST API reverse proxy. It performs load balance,
+//! caching, and health check; and also prevents DDOS and reports metrics 
+//! concerning health status of backend servers.
+//! 
+// ----------------------------- bring Modules -------------------------------
